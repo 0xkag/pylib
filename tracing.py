59628541081cef609e951e38f398dc7a31374dba
Kyle George <kgeorge@tcpsoft.com>
2012-10-06 00:26:11 -0400

    ** adding a link in the comment

--- tracing.py
+++ tracing.py
@@ -5,6 +5,8 @@
 #
 # Copyright (c) 2012 Kyle George <kgeorge@tcpsoft.com>
 #
+# Homepage: https://github.com/k-a-g/pylib
+#
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
